ab29f348671243e9a9fbfa6c97e7e06c7da94726
vinayjallipalli <vinayjallipalli@gmail.com>
2016-12-08 14:56:18 +0530

    a2

---
+++ includes/navigation.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title>Srinivasa Manure Depot</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" type="text/css" href="style/sty1.css">
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Delicious">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+
+<body>
+
+
+<nav class="navbar navbar-inverse">
+	  <div class="container-fluid">
+	    
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>                        
+	      </button>
+	      <a class="navbar-brand" href="#">Srinivasa Manure Depot</a>
+	    </div>
+
+	    <div class="collapse navbar-collapse" id="myNavbar">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="index.php">Home</a></li>
+	        <li><a href="Pest.php">Pesticides</a></li>
+	        <li><a href="Fert.php">Fertilizers</a></li>
+	        <li><a href="Cement.php">Cement</a></li>
+	      </ul>	      
+	    </div>
+
+	  </div>
+</nav>
+
+
+
+
+</body>
+
+
+</html>
